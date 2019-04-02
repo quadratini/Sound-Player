24ae01bee9d1d580f2b4a45cb0a287f2c55f69e5
Ronnapob Ritprasert <ronnyrit@gmail.com>
2019-04-02 16:05:44 -0700

    Created SoundMain Added Frame

---
+++ SoundMain.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class SoundMain {
+
+    public static void main(String args[]) {
+        initFrame();
+    }
+
+
+
+    public static void initFrame() {
+        JFrame frame = new JFrame("Sound Player");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+
+
+        JButton playButton = new JButton("Play");
+        frame.add(playButton);
+
+
+        frame.setVisible(true);
+
+    }
+
+
+}
+
