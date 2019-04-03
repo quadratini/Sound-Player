2de0b03c7f0196a788a738456bae0406b88d7e82
Ronnapob Ritprasert <ronnyrit@gmail.com>
2019-04-02 21:25:45 -0700

    whoops

--- SoundMain.java
+++ SoundMain.java
@@ -1,4 +1,9 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.util.ArrayList;
 
 public class SoundMain {
 
@@ -6,18 +11,44 @@ public class SoundMain {
         initFrame();
     }
 
+    private static void initFrame() {
+        GridBagConstraints c = new GridBagConstraints();
+        JPanel panel = new JPanel(new GridBagLayout());
+        JPanel flow = new JPanel(new FlowLayout());
+        JLabel songName = new JLabel("Test");
 
+        panel.setBackground(Color.RED);
+        flow.setBackground(Color.DARK_GRAY);
 
-    public static void initFrame() {
+        c.insets = new Insets(2, 2, 2, 2);
+        c.gridx = 0;
+        c.gridy = 0;
+
         JFrame frame = new JFrame("Sound Player");
+        frame.setLayout(new GridBagLayout());
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(500,500);
 
-
         JButton playButton = new JButton("Play");
-        frame.add(playButton);
+        JButton nextButton = new JButton("Next");
+        JButton previousButton = new JButton("Previous");
 
+        JTextPane textPane = new JTextPane();
+        textPane.setPreferredSize(new Dimension(200,400));
+        textPane.setEditable(false);
 
+        c.gridwidth = 2;
+        c.gridheight = 2;
+        panel.add(textPane, c);
+        textPane.add(songName);
+
+        flow.add(previousButton);
+        flow.add(playButton);
+        flow.add(nextButton);
+
+        frame.add(panel);
+        c.gridy = 1;
+        frame.add(flow, c);
         frame.setVisible(true);
 
     }
